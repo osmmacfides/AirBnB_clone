bb1469e1f331811cd61ec596d650f0d9f9b851e9
caramelinvestor <caramelinvestor@gmail.com>
2023-09-13 07:44:13 -0700

    Task 4

--- models/base_model.py
+++ models/base_model.py
@@ -13,13 +13,26 @@ class BaseModel:
     for other classes
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Instantiation method
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+
+            if "created_at" in kwargs:
+                self.created_at = datetime.strptime(kwargs["created_at"],
+                                                    "%Y-%m-%dT%H:%M:%S.%f")
+            if "updated_at" in kwargs:
+                self.updated_at = datetime.strptime(kwargs["updated_at"],
+                                                    "%Y-%m-%dT%H:%M:%S.%f")
+
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
